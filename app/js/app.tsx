1f0861dd6fde61f2a5b3a6fa46efbeb8992efe88
samhcorney <samcorney@Sam-Corneys-iMac.local>
2016-09-23 18:15:52 +0100

    Removed page container

--- app/js/app.tsx
+++ app/js/app.tsx
@@ -2,16 +2,53 @@ import 'svgxuse';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { PageContainer } from './components/PageContainer';
+/*
+ * Components
+ */
+import { SideMenu } from './components/SideMenu';
+import { PrimaryContentContainer } from './components/PrimaryContentContainer';
 
-export class MyApp {
+/*
+ * Models
+ */
+import { MenuItem } from "./models/MenuItem"
 
-    constructor () {
-        ReactDOM.render(
-            <PageContainer />,
-            document.getElementById( 'content' )
+export class MyApp extends React.Component<any, {}> {
+
+    menuItems: MenuItem[] = [
+        { title: 'Site Editor', handle: 'siteEditor' },
+        { title: 'Content Editor', handle: 'contentEditor' },
+        { title: 'Palette', handle: 'palette' }
+    ];
+    activeMenuItem: MenuItem = this.menuItems[0];
+
+    constructor( props : any ) {
+        super( props );
+        this.handleMenuItemClick = this.handleMenuItemClick.bind( this );
+    }
+
+    handleMenuItemClick ( menuItem: MenuItem ) {
+        this.activeMenuItem = menuItem;
+        this.setState( this.activeMenuItem );
+    }
+
+    render () {
+        return (
+            <div className="pageContainer">
+                <SideMenu menuItems={ this.menuItems } onMenuItemClick={ this.handleMenuItemClick } />
+                <PrimaryContentContainer menuItem={ this.activeMenuItem } />
+            </div>
         );
     }
 }
 
-this.MyApp();
+
+
+
+/*
+ * Render the application to the DOM using react
+ */
+ReactDOM.render(
+    <MyApp />,
+    document.getElementById( 'content' )
+);
