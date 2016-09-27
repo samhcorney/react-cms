3ae25c400284bf6f07b8cd81b3f46e03507fe9e1
samhcorney <samcorney@Sam-Corneys-iMac.local>
2016-09-27 19:46:28 +0100

    Add/remove content

--- README.md
+++ README.md
@@ -4,4 +4,21 @@
 * Complete rewrite of backend ( all code is currently for testing ). Make sure to remove need for temporaryhack when rewriting theme saving logic
 * Add document store db ( couchdb / mongodb )
 * Allow for easy creation of custom type ( no modification to core )
-* Add list type with option in cms to add remove
+
+
+
+* Add list type with option in cms to add remove from list and sort. You should be able to add any content types to a list. Sorting should be smart enough to work out which unique values exist in each of these content types and sort accordingly.
+
+
+
+
+
+* SVGs
+* Make remove work for nested json objects
+* Make add work for nested json objects
+* Validation on add. Handle must be unigue ( at least for that current depth in the JSON tree ). Name can't be blank.
+* Move add content logic into separate component ( need a way of restricting which content types are available... i.e. for theme only colour ). Current array of content types should be set once and used to produce the content imports. i.e. the current switch statement over all content should use this. This also needs to be extensible with custom content through plugins/ custom contentType creation per site.
+* Basic 'reset' styles for select type
+* Make content sortable/arrangeable through drag and drop on the current list of content items.
+* Apple S to save. Enter to save?
+* Reusable toast component. To appear on save success/failure.
--- app/js/components/ContentEditor.tsx
+++ app/js/components/ContentEditor.tsx
@@ -5,14 +5,62 @@ import { TextArea } from './contenttypes/TextArea';
 import { Number } from './contenttypes/Number';
 import { Checkbox } from './contenttypes/Checkbox';
 
+const contentTypes = [
+    { name: 'Text', handle: 'text' },
+    { name: 'Text Area', handle: 'textarea' },
+    { name: 'Number', handle: 'number' },
+    { name: 'Checkbox', handle: 'checkbox' },
+    { name: 'Colour', handle: 'colour' }
+ ];
+
 export class ContentEditor extends React.Component<any, {}> {
+
+    state: any = { addContentHandle: '', addContentType: contentTypes[0].handle, addContentName: "" };
 
     constructor( props : any ) {
         super( props );
     }
 
+    componentWillMount () {
+
+        this.setState( this.state );
+    }
+
     handleChange () {
 
+        this.props.onContentChange( this.props.content );
+    }
+
+    removeContent ( event ) {
+
+        delete this.props.content[ event.target.id ];
+        this.props.onContentChange( this.props.content );
+    }
+
+    handleAddContentNameChange ( event ) {
+
+        this.setState( { addContentName: event.target.value } );
+    }
+
+    handleAddContentTypeChange ( event ) {
+
+        this.setState( { addContentType: event.target.value } );
+    }
+
+    handleAddContentHandleChange ( event ) {
+
+        this.setState( { addContentHandle: event.target.value } );
+    }
+
+    addContent ( event ) {
+
+        let newContent = {
+            _type: this.state.addContentType,
+            _name: this.state.addContentName,
+            _content: ""
+        };
+        this.props.content[ this.state.addContentHandle ] = newContent;
+        this.setState( { addContentHandle: '', addContentType: contentTypes[0].handle, addContentName: "" } );
         this.props.onContentChange( this.props.content );
     }
 
@@ -27,37 +75,62 @@ export class ContentEditor extends React.Component<any, {}> {
 
             switch( allContent[key]._type ) {
                 case 'text':
-                    content = <Text content={ allContent[key] } id={ key } onContentChange={ this.handleChange.bind( this ) } />;
+                    content = <Text content={ allContent[key] } onContentChange={ this.handleChange.bind( this ) } />;
                     break;
                 case 'textarea':
-                    content = <TextArea content={ allContent[key] } id={ key } onContentChange={ this.handleChange.bind( this ) } />;
+                    content = <TextArea content={ allContent[key] } onContentChange={ this.handleChange.bind( this ) } />;
                     break;
                 case 'number':
-                    content = <Number content={ allContent[key] } id={ key } onContentChange={ this.handleChange.bind( this ) } />;
+                    content = <Number content={ allContent[key] } onContentChange={ this.handleChange.bind( this ) } />;
                     break;
                 case 'checkbox':
-                    content = <Checkbox content={ allContent[key] } id={ key } onContentChange={ this.handleChange.bind( this ) } />;
+                    content = <Checkbox content={ allContent[key] } onContentChange={ this.handleChange.bind( this ) } />;
                     break;
                 case 'colour':
-                    content = <Text content={ allContent[key] } id={ key } onContentChange={ this.handleChange.bind( this ) } />;
+                    content = <Text content={ allContent[key] } onContentChange={ this.handleChange.bind( this ) } />;
                     break;
                 default:
-                    content = <Text content={ allContent[key] } id={ key } onContentChange={ this.handleChange.bind( this ) } />;
+                    content = <Text content={ allContent[key] } onContentChange={ this.handleChange.bind( this ) } />;
             }
 
             contentItems.push (
                 <div className="contentCard" key={ key }>
                     <div className="contentCard-container">
-                        <h4 className="contentCard-label">{ allContent[key]._name }</h4>
+                        <h4 className="contentCard-label">{ allContent[key]._name } ( { key } )</h4>
                         <div className="contentCard-content">
                             { content }
                         </div>
+                        <h4 className="contentCard-remove" id={ key } onClick={ this.removeContent.bind( this ) }>Remove</h4>
                     </div>
                 </div>
             )
         }
+
+        let contentTypeOptions = [];
+
+        for ( var key in contentTypes ) {
+            contentTypeOptions.push(
+                <option value={ contentTypes[ key ].handle } key={ key }>{ contentTypes[ key ].name }</option>
+            )
+        }
+
         return (
-            <div className="contentEditor">{ contentItems }</div>
+            <div className="contentEditor">
+                { contentItems }
+                <div className="contentCard">
+                    <h4>Name</h4>
+                    <input type="text" value={ this.state.addContentName } onChange={ this.handleAddContentNameChange.bind( this ) } />
+
+                    <h4>Handle</h4>
+                    <input type="text" value={ this.state.addContentHandle } onChange={ this.handleAddContentHandleChange.bind( this ) } />
+
+                    <h4>Type</h4>
+                    <select value={ this.state.addContentType } onChange={ this.handleAddContentTypeChange.bind( this ) }>
+                        { contentTypeOptions }
+                    </select>
+                    <button onClick={ this.addContent.bind( this ) }>Add</button>
+                </div>
+            </div>
         );
     }
 }
